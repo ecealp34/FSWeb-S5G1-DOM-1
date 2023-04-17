1c2c6c05dba34b8b84a5182237737557ed1c452d
Ece <ecekalpelli@gmail.com>
2023-04-17 23:36:33 +0300

    s5g1

--- src/index.js
+++ src/index.js
@@ -43,3 +43,85 @@ console.log('Proje açıldı!')
 
 
 /* Kodlar Buradan aşağıya */
+
+
+document.getElementsByTagName("a")[0].innerHTML = "Servisler"
+
+document.getElementsByTagName("a")[0].className = "italic"
+
+document.getElementsByTagName("a")[1].innerHTML = "Ürünler"
+
+document.getElementsByTagName("a")[1].className = "italic"
+
+document.getElementsByTagName("a")[2].innerHTML = "Vizyon"
+
+document.getElementsByTagName("a")[2].className = "italic"
+
+document.getElementsByTagName("a")[3].innerHTML = "Özellikler"
+
+document.getElementsByTagName("a")[3].className = "italic"
+
+document.getElementsByTagName("a")[4].innerHTML = "Hakkımızda"
+
+document.getElementsByTagName("a")[4].className = "italic"
+
+document.getElementsByTagName("a")[5].innerHTML = "İletişim"
+
+document.getElementsByTagName("a")[5].className = "italic"
+
+document.getElementsByTagName("a")[6].innerHTML = "Copyright Bir Şirket Sitesi 2022"
+
+document.getElementsByTagName("a")[6].className = "bold"
+
+function cta(h1, button) {
+
+h1 = document.createElement("h1").innerText = "Bu DOM Mükemmel";
+
+button = document.createElement("button").innerText = "Başlayın";
+
+}
+
+document.getElementsByTagName("h4")[0].innerHTML = "Özellikler"
+
+document.getElementsByTagName("p")[0].innerHTML = "Özellikler içeriği elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio,in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
+document.getElementsByTagName("h4")[1].innerHTML = "Hakkında"
+
+document.getElementsByTagName("p")[1].innerHTML = "Hakkında içeriği elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, ininterdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metusscelerisque quis."
+
+document.getElementsByTagName("h4")[2].innerHTML = "Servisler"
+
+document.getElementsByTagName("p")[2].innerHTML = "Servisler içeriği elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio,in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metusscelerisque quis."
+
+document.getElementsByTagName("h4")[3].innerHTML = "Ürünler"
+
+document.getElementsByTagName("p")[3].innerHTML = "Ürünler içeriği elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, ininterdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metusscelerisque quis."
+
+document.getElementsByTagName("h4")[4].innerHTML = "Vizyon"
+
+document.getElementsByTagName("p")[4].innerHTML = "Vizyon içeriği elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
+document.getElementsByTagName("h4")[5].innerHTML = "İletişim"
+
+document.getElementsByTagName("p")[5].innerHTML = "100 numara Bilmem ne sokak Ankara'da bir semt, Türkiye"
+
+document.getElementsByTagName("p")[6].innerHTML = "+90 (123) 456-7899"
+
+document.getElementsByTagName("p")[7].innerHTML = "satis@birsirketsitesi.com.tr"
+
+
+document.getElementById('logo-img').src = "http://localhost:9000/img/logo.png";
+
+document.getElementById('cta-img').src = "http://localhost:9000/img/cta.png";
+
+document.getElementById('middle-img').src = "http://localhost:9000/img/accent.png";
+
+
+
+
+
+
+
+
+
+
